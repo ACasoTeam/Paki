392c6b8659cba526c87351d8130c0b9e5f95225a
Biggamehunt <treasurehuntturin@gmail.com>
2016-12-12 00:19:14 +0100

    popo

--- app/src/main/java/acasoteam/pakistapp/MainActivity.java
+++ app/src/main/java/acasoteam/pakistapp/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.print("random");
         System.out.print("random2");
+        System.out.print("random3");
 
     }
 }
