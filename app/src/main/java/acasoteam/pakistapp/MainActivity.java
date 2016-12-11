517c000379df454c6c8133a40dee7a4c5ae322e2
David <marsili.david@gmail.com>
2016-12-11 22:40:36 +0100

    Commit test - David

---
+++ app/src/main/java/acasoteam/pakistapp/MainActivity.java
@@ -0,0 +1,13 @@
+package acasoteam.pakistapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// test
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
